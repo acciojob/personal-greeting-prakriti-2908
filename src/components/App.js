34d86c082b2807490f0dbb6d0c0d03bf89de7d1b
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-05-14 17:43:08 +0000

    try2

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [para,setPara] = useState("");
+  const [para,setPara] = useState('');
 
   const about = (event) => {
     setPara(event.target.value);
   }
   return (
     <div>
-      <p>Enter your name:</p>
+      {/* <p>Enter your name:</p> */}
       <input type="text" value={para} onClick={about}></input>
       <p>Hello {para}!</p>
     </div>
